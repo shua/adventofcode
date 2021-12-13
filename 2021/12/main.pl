b5c0128b6c189af715784390006c6814274f5893
shua <itis@isthisa.email>
2021-12-13 06:53:08 -0500

    day 12 answer 2

--- 2021/12/main.pl
+++ 2021/12/main.pl
@@ -13,55 +13,59 @@ input(Es) :-
 
 sample([start-'A',start-b,'A'-c,'A'-b,b-d,'A'-end,b-end]).
 
+reverse_elems([], []).
+reverse_elems([E|Es], [Er|Ers]) :- reverse(E, Er), reverse_elems(Es, Ers).
+
 % recursive depth-firsh search
 % paths from S-E
 % SE = edges from S-_ in Es
 % Pi = for S-Ei in SE . edges_without(Es, 
+%
+% NOTE(jd): runtime for answer1 is around 1.6s, tried tabling, but it was much worse
 paths(Es, Start, End, Ps) :-
-	paths(Es, [Start], End, [], Ps).
+	paths(Es, [Start], End, [], Ps0),
+	reverse_elems(Ps0, Ps).
 paths(_, [], _, _, []).
+paths(Es, [N|Ns], End, Path, [[N|Path]|Paths]) :-
+	N == End,
+	paths(Es, Ns, End, Path, Paths).
 paths(Es, [N|Ns], End, Path, Paths) :-
-	(	N = End
-	->	reverse([N|Path], CompletePath),
-		SubPaths = [CompletePath]
-	;	visit_vertex(Es, N, VNext, EInt),
-		rm_lowercase(EInt, [N], ENext),
+	N \== End,
+	visit_vertex(Es, N, VNext),
+	(	lowercase(N),
+		rm_vertex(Es, N, ENext),
 		paths(ENext, VNext, End, [N|Path], SubPaths)
+	;	uppercase(N),
+		paths(Es, VNext, End, [N|Path], SubPaths)
 	),
 	paths(Es, Ns, End, Path, OtherPaths),
 	append(SubPaths, OtherPaths, Paths).
 
 % Edges, Vertex, ConnectedVerts, 
-visit_vertex(Es, V, VNext, ENext) :-
-	visit_vertex_(Es, V, VNext0, ENext),
+visit_vertex(Es, V, VNext) :-
+	visit_vertex_(Es, V, VNext0),
 	sort(VNext0, VNext).
-visit_vertex_([], _, [], []).
-visit_vertex_([V1-V2|Edges], Vert, VNext, ENext) :-
-	(V1 = Vert, VNext = [V2|VNextn] ; V2 = Vert, VNext = [V1|VNextn])
-->	ENext = [V1-V2|ENextn],
-	visit_vertex_(Edges, Vert, VNextn, ENextn)
-;	ENext = [V1-V2|ENextn],
-	visit_vertex_(Edges, Vert, VNext, ENextn).
+visit_vertex_([], _, []).
+visit_vertex_([V1-V2|Edges], Vert, VNext) :-
+	(	V1 == Vert, VNext = [V2|VNextn]
+	;	V2 == Vert, VNext = [V1|VNextn]
+	),
+	visit_vertex_(Edges, Vert, VNextn)
+;	V1 \== Vert, V2 \== Vert, visit_vertex_(Edges, Vert, VNext).
 
 % codes for lowercase 'a' and 'z'
 lowercase(Atom) :- atom_codes(Atom, [C|_]), C >= 97, C =< 122.
+uppercase(Atom) :- atom_codes(Atom, [C|_]), C >= 65, C =< 90.
 
-just_lowercase([], []).
-just_lowercase([A|As], Bs) :-
-	lowercase(A)
-->	just_lowercase(As, BNext), Bs = [A|BNext]
-;	just_lowercase(As, Bs).
-
-rm_lowercase(Es, Vs, EOut) :-
-	just_lowercase(Vs, VLower),
-	rm_vertices(Es, VLower, EOut).
-rm_vertices([], _, []).
-rm_vertices([V1-V2|Es], Vs, EOut) :-
-	(	(member(V1, Vs) ; member(V2, Vs))
-	->	EOut = ENext
-	;	EOut = [V1-V2|ENext]
-	),
-	rm_vertices(Es, Vs, ENext).
+rm_lowercase(Es, V, EOut) :- lowercase(V), rm_vertex(Es, V, EOut).
+rm_lowercase(Es, V, Es) :- uppercase(V).
+rm_vertex([], _, []).
+rm_vertex([V1-V2|Es], V, EOut) :-
+	(V1 == V ; V2 == V),
+	rm_vertex(Es, V, EOut).
+rm_vertex([V1-V2|Es], V, [V1-V2|EOut]) :-
+	V1 \== V, V2 \== V,
+	rm_vertex(Es, V, EOut).
 
 write_paths([]).
 write_paths([P|Ps]) :- write(P), write('\n'), write_paths(Ps).
@@ -73,21 +77,36 @@ answer1(N, Es, Ps) :-
 	length(Ps, N).
 
 paths2(Es, Start, End, Ps) :-
-	paths2(Es, [Start], End, [], Ps).
+	paths2(Es, [Start], End, [], Ps0),
+	reverse_elems(Ps0, Ps1),
+	% rm dups
+	% path2(a,path2(b,path(c))) vs path2(a,path(b,path(c))) both give [a,b,c]
+	sort(Ps1, Ps).
 paths2(_, [], _, _, []).
+paths2(Es, [N|Ns], End, Path, [[N|Path]|Paths]) :-
+	N == End,
+	paths2(Es, Ns, End, Path, Paths).
 paths2(Es, [N|Ns], End, Path, Paths) :-
-	(	N = End
-	->	reverse([N|Path], CompletePath),
-		SubPaths = [CompletePath]
-	;	visit_vertex(Es, N, VNext, EInt),
-		rm_lowercase(EInt, [N], ENext),
-		paths2(ENext, VNext, End, [N|Path], SubPaths)
-	),
+	N \== End,
+	visit_vertex(Es, N, VNext),
+	% regular
+	rm_lowercase(Es, N, ENext),
+	paths2(ENext, VNext, End, [N|Path], SubPaths1),
 	paths2(Es, Ns, End, Path, OtherPaths),
+	% leave one lowercase in, but then proceed with original paths
+	(	lowercase(N), N \== start,
+		paths(Es, VNext, End, [N|Path], SubPaths2),
+		append(SubPaths2, SubPaths1, SubPaths)
+	;	(uppercase(N) ; N == start),
+		SubPaths = SubPaths1
+	),
 	append(SubPaths, OtherPaths, Paths).
 
+% 3m30s not ideal, but got the right answer :shrug:
+answer2(N) :- answer2(N, _, _).
 answer2(N, Es, Ps) :-
 	input(Es),
-	time(paths2(Es, start, end, Ps)),
+	time(paths2(Es, start, end, Ps0)),
+	sort(Ps0, Ps),
 	length(Ps, N).
 	
