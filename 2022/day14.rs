9002b0c64ab1c8fb610f0525041563f18db8c7ec
shua <itis@isthisa.email>
2022-12-14 14:13:23 +0100

    2022 day14 part 2

--- 2022/day14.rs
+++ 2022/day14.rs
@@ -12,13 +12,13 @@ enum Mat {
     Sand,
 }
 
-fn printmap(min: (usize, usize), (width, height): (usize, usize), map: &[Mat]) {
-    println!("min {min:?}, width {width} height {height}");
+fn printmap((minx, miny): (usize, usize), (width, height): (usize, usize), map: &[Mat]) {
+    println!("minx {minx} miny {miny} width {width} height {height}");
 
-    for y in min.1..height {
+    for y in miny..height {
         let mut line = String::new();
         line.push('|');
-        for x in min.0..width {
+        for x in minx..width {
             match map[x + width * y] {
                 Mat::Rock => line.push('#'),
                 Mat::Air => line.push(' '),
@@ -31,7 +31,9 @@ fn printmap(min: (usize, usize), (width, height): (usize, usize), map: &[Mat]) {
 
 fn step(map: &mut [Mat], (width, height): (usize, usize), (x, y): (usize, usize)) -> bool {
     let row = width*(y+1);
-    match (map.get((x-1)+row), map.get(x + row), map.get((x+1) + row)) {
+    let left = if x == 0 { None } else { map.get((x-1)+row) };
+    let right = if x >= width-1 { None } else { map.get((x+1)+row) };
+    match (left, map.get(x + row), right) {
         (_, None, _) => true,
         (_, Some(Mat::Air), _) => step(map, (width, height), (x, y+1)),
         (None, Some(_), _) => true,
@@ -41,6 +43,33 @@ fn step(map: &mut [Mat], (width, height): (usize, usize), (x, y): (usize, usize)
         (Some(_), Some(_), Some(_)) => {
             map[x+width*y] = Mat::Sand;
             false
+        }
+    }
+}
+
+fn step2(map: &mut [Mat], (width, height): (usize, usize), (x, y): (usize, usize)) -> usize {
+    let row = width*(y+1);
+    let left = if x == 0 { None } else { map.get((x-1)+row) };
+    let right = if x >= width - 1 { None } else { map.get((x+1)+row) };
+    match (left, map.get(x + row), right) {
+        (_, None, _) => {
+            map[x+width*y] = Mat::Sand;
+            1
+        },
+        (_, Some(Mat::Air), _) => step2(map, (width, height), (x, y+1)),
+        (None, Some(_), _) => {
+            map[x+width*y] = Mat::Sand;
+            height-y
+        },
+        (Some(Mat::Air), Some(_), _) => step2(map, (width, height), (x-1, y+1)),
+        (Some(_), Some(_), None) => {
+            map[x+width*y] = Mat::Sand;
+            height-y
+        },
+        (Some(_), Some(_), Some(Mat::Air)) => step2(map, (width, height), (x+1, y+1)),
+        (Some(_), Some(_), Some(_)) => {
+            map[x+width*y] = Mat::Sand;
+            1
         }
     }
 }
@@ -69,33 +98,49 @@ fn main() {
             parsed.push(path);
         }
     }
+    min = (min.0 - height, 0);
+    width = (width+1) - min.0;
     let mut map = vec![Mat::Air; width * height];
-    map[500 + width*0] = Mat::Sand; 
+    map[(500-min.0) + width*0] = Mat::Sand; 
     for path in parsed {
         for pair in path.windows(2) {
             let (a, b) = (pair[0], pair[1]);
-            fn rng(s: usize, e: usize) -> std::ops::RangeInclusive<usize> {
+            fn rng(base: usize, s: usize, e: usize) -> std::ops::RangeInclusive<usize> {
                 if s > e {
-                    e..=s
+                    (e-base)..=(s-base)
                 } else {
-                    s..=e
+                    (s-base)..=(e-base)
                 }
             }
-            for x in rng(a.0, b.0) {
-                for y in rng(a.1, b.1) {
+            for x in rng(min.0, a.0, b.0) {
+                for y in rng(min.1, a.1, b.1) {
                     map[x + width * y] = Mat::Rock;
                 }
             }
         }
     }
 
-    printmap(min, (width, height), &map);
-
     let mut i = 0;
-    while !step(&mut map, (width, height), (500, 0)) {
+    while !step(&mut map, (width, height), (500-min.0, 0)) {
         i += 1;
-        if i % 50 == 0 { println!("step: {i} "); printmap(min, (width, height), &map); }
     }
     println!("total units: {i}");
-    printmap(min, (width, height), &map);
+    printmap((height, min.1), (width, height), &map);
+    
+    map[(500-min.0)+width*0] = Mat::Air;
+    map.extend(vec![Mat::Air; width]);
+    height += 1;
+    
+    let mut last = i;
+    while map[(500-min.0)+width*0] != Mat::Sand {
+        let s = step2(&mut map, (width, height), (500-min.0, 0));
+        i += s;
+    }
+    println!("total units: {i}");
+    printmap((height, min.1), (width, height), &map);
+    
+    let vis = map.iter().filter(|m| **m == Mat::Sand).count();
+    let invis = map.iter().enumerate().filter(|(i, m)| (((i+1) % width) == 0) && **m == Mat::Sand).count() - 1;
+    let invis = (invis * (invis + 1)) / 2;
+    assert!(i == vis + invis);
 }
